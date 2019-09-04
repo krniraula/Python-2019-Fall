3b01a32fa6f8cc3f1c1f0190d482062d7642c8f3
Khem Niraula <khemrajniraula@gmail.com>
2019-09-03 21:28:22 -0500

    working with numbwes

---
+++ InClasswork/working _with_variables.py
@@ -0,0 +1,8 @@
+message = "hello_world"
+print (Message)
+message__2 = hello_world 
+print(message_2)
+
+hello_world_message = hello_qorld_Message + "Khem Niraula"
+print hello_world_Message
+
---
+++ InClasswork/working_with_numbers.py
@@ -0,0 +1,12 @@
+age = 50
+
+disciples_Count = 12
+
+cost_of__coffee
+
+pi = 3.131592654
+
+ciercumference = 2 * pi * redius
+
+print("cercumference"+ ciercumference)
+
