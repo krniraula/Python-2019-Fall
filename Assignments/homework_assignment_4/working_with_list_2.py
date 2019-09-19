953f5f07f993a770484470af7ab52eae44eb9836
Khem Niraula <khemrajniraula@gmail.com>
2019-09-19 11:01:11 -0500

    python

---
+++ Assignments/homework_assignment_4/working_with_list_2.py
@@ -0,0 +1,10 @@
+#Name: Khem Niraula
+#Student ID: 0644115
+#Due Date: September 22, 2019
+#MSITM 6341
+
+odd_numbers = list(range(3,51,2))
+print(odd_numbers)
+x = 0
+for x in range(0,5):
+    print(odd_numbers[x])
