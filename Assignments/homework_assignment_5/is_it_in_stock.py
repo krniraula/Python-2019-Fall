e84e9a3c4da36b0d72c714637f2f874d5d7807d7
Khem Niraula <khemrajniraula@gmail.com>
2019-09-29 16:17:38 -0500

    pp

--- Assignments/homework_assignment_5/is_it_in_stock.py
+++ Assignments/homework_assignment_5/is_it_in_stock.py
@@ -9,14 +9,14 @@ customer_order = ['Tacos', 'Guacamole', 'Burrito', 'Chips', 'Salsa']
 
 #converting both the lists to lower case using lower keyword
 
-menu_items_in_stock_lower =[dish.lower() for dish in menu_items_in_stock]
-customer_order_lower = [dish.lower() for dish in customer_order]
-print(menu_items_in_stock_lower)
-print(customer_order_lower)
+menu_items_in_stock_lower =[item.lower() for item in menu_items_in_stock]
+customer_order_lower = [item.lower() for item in customer_order]
+#print(menu_items_in_stock_lower)
+#print(customer_order_lower)
 
-#checking whether the customer_order is available in stocks for elements in customer_order:
-for item_ordered in customer_order:
-    if item_ordered in menu_items_in_stock:
-        print('We have ' + item_ordered + ' in stock' + '.')
+for i in range (0,len(customer_order_lower)):
+    if customer_order_lower[i] in menu_items_in_stock_lower:
+        print("We have", customer_order[i],"in stock.")
     else:
-        print('We do not have ' + item_ordered + ' in stock' + '.')
+        print("We do not have",customer_order[i],"in stock.")
+
---
+++ InClasswork/quiz_4_new.py
@@ -0,0 +1,11 @@
+#grocery_items = ['Apples', 'Oranges', 'Pineapple']        
+#for x in grocery_items:        
+    #print(x)
+    #print(x) 
+#print(x)
+#grocery_items = ['Apples', 'Oranges', 'Pineapple']    
+#for x in grocery_items:        
+    #print(x)    
+#print(x)        
+    #print(x)
+print(grocery_items[:8])
