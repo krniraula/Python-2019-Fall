10f2328c04985546721a03877782e348aad04705
Khem Niraula <khemrajniraula@gmail.com>
2019-11-05 20:33:35 -0600

    python

---
+++ Attendance/check_string_numbers.py
@@ -0,0 +1,17 @@
+#Name: Khem Niraula
+#Student ID: 0644115
+#Due Date: November 10, 2019
+#MSITM 6341
+
+# Problem of the day
+def check_num(num1,num2):
+    #first_num = str(input("enter First No.:"))
+    #Second_num = str(input("enter Second No.:"))
+    if int(num1)==int(num2):
+        print("The Numbers are equal..")
+    else:
+        print("The Numbers are not equal..")
+num1=input("Enter first num: ")
+num2=input("Enter Second num: ")
+
+check_num(num1,num2)
